17768a12c554fcac15c1d03d837e6ba0d543d3ed
arjnai21 <arjun@nairsnet.com>
2019-07-30 18:14:24 -0400

    update

--- intro_and_data.py
+++ intro_and_data.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import math
 import quandl
+import numpy as np
+from sklearn import preprocessing, model_selection, svm
+from sklearn.linear_model import LinearRegression
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -17,9 +20,24 @@ forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 
 forecast_out = int(math.ceil(0.01 * len(df)))
+print(forecast_out)
 
 df['label'] = df[forecast_col].shift(-forecast_out)
+
+
+X = np.array(df.drop(['label'], 1))
+X = X[:-forecast_out]
+X = preprocessing.scale(X)
+X_lately = X[-forecast_out:]
 df.dropna(inplace=True)
-print(df.head())
 
+y = np.array(df['label'])
+y = np.array(df['label'])
 
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
+
+clf = LinearRegression(n_jobs=-1)
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
+
+print(accuracy)
